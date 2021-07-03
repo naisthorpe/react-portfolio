0b3d0e842dc1f1ba94acc23d5f3433161cd168f4
naisthorpe <aisthorpe@gmail.com>
2021-07-03 11:51:51 -0400

    Little more styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -19,7 +19,7 @@ function Header() {
                                 <a className="nav-link text-light" href="/contact">Contact</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-light" href="#">Resume</a>
+                                <a className="nav-link text-light" href="resume/Aisthorpe, Nicholas - Resume 2021.pdf" download>Resume</a>
                             </li>
                         </ul>
                     </div>
